4790112fc2e8a483dc400d00091aceeaa3387dfe
jeanbisutti <jean.bisutti@gmail.com>
2022-09-14 17:52:41 +0200

    Start-up profiler: project initialization

---
+++ startup-profiler/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+  id("otel.java-conventions")
+}
+
+dependencies {
+  implementation("com.microsoft.jfr:jfr-streaming:1.2.0")
+}
+
+
+tasks {
+  jar {
+    manifest {
+      attributes(
+        "Premain-Class" to "io.opentelemetry.javaagent.OpenTelemetryAgent",
+      )
+    }
+  }
+}
---
+++ startup-profiler/src/main/java/io/opentelemetry/contrib/startupprofiler/StartupProfilerAgent.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.contrib.startupprofiler;
+
+import java.lang.instrument.Instrumentation;
+
+public class StartupProfilerAgent {
+
+  public static void premain(String agentArgs, Instrumentation inst) {
+
+  }
+
+}
