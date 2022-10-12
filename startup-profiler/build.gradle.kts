160ec91edb96b7a8fbddb064dda00f1426dd133e
Jean Bisutti <jean.bisutti@gmail.com>
2022-10-12 11:20:09 +0200

    Include JFR client into startup profiler jar

--- startup-profiler/build.gradle.kts
+++ startup-profiler/build.gradle.kts
@@ -2,8 +2,14 @@ plugins {
   id("otel.java-conventions")
 }
 
+val jfrClient: Configuration by configurations.creating {
+  isCanBeResolved = true
+  isCanBeConsumed = false
+}
+
 dependencies {
   implementation("com.microsoft.jfr:jfr-streaming:1.2.0")
+  jfrClient("com.microsoft.jfr:jfr-streaming:1.2.0")
 }
 
 
@@ -14,5 +20,9 @@ tasks {
         "Premain-Class" to "io.opentelemetry.contrib.startupprofiler.StartupProfilerAgent",
       )
     }
+    inputs.files(jfrClient)
+    from({
+      jfrClient.singleFile
+    })
   }
 }
