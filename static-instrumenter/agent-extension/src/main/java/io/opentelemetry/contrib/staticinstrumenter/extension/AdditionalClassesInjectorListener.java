e28e65896543bea4c0e1e674405628d81c2fb8d8
Jean Bisutti <jean.bisutti@gmail.com>
2022-10-14 16:47:11 +0200

    Document additional classes

--- static-instrumenter/agent-extension/src/main/java/io/opentelemetry/contrib/staticinstrumenter/extension/AdditionalClassesInjectorListener.java
+++ static-instrumenter/agent-extension/src/main/java/io/opentelemetry/contrib/staticinstrumenter/extension/AdditionalClassesInjectorListener.java
@@ -12,6 +12,18 @@ import java.util.Map;
 /**
  * A listener to be registered in {@link io.opentelemetry.javaagent.tooling.HelperInjector}. It
  * saves all additional classes created by the agent to the AdditionalClasses class.
+ *    /*
+ *     Additional class: io/opentelemetry/javaagent/bootstrap/field/VirtualFieldAccessor$java$util$concurrent$Callable$io$opentelemetry$javaagent$bootstrap$executors$PropagatedContext.class
+ *     Additional class: io/opentelemetry/javaagent/bootstrap/field/VirtualFieldAccessor$java$lang$Runnable$io$opentelemetry$javaagent$bootstrap$executors$PropagatedContext.class
+ *     Additional class: io/opentelemetry/javaagent/bootstrap/field/VirtualFieldAccessor$java$util$concurrent$Future$io$opentelemetry$javaagent$bootstrap$executors$PropagatedContext.class
+ *     Additional class: io/opentelemetry/javaagent/bootstrap/field/VirtualFieldAccessor$java$util$concurrent$ForkJoinTask$io$opentelemetry$javaagent$bootstrap$executors$PropagatedContext.class
+ *     Additional class: io/opentelemetry/javaagent/bootstrap/field/VirtualFieldImpl$java$util$concurrent$ForkJoinTask$io$opentelemetry$javaagent$bootstrap$executors$PropagatedContext.class
+ *     Additional class: io/opentelemetry/javaagent/bootstrap/field/VirtualFieldImpl$java$util$concurrent$Callable$io$opentelemetry$javaagent$bootstrap$executors$PropagatedContext.class
+ *     Additional class: io/opentelemetry/javaagent/bootstrap/field/VirtualFieldImpl$java$util$concurrent$Future$io$opentelemetry$javaagent$bootstrap$executors$PropagatedContext.class
+ *     Additional class: io/opentelemetry/javaagent/bootstrap/field/VirtualFieldImpl$java$lang$Runnable$io$opentelemetry$javaagent$bootstrap$executors$PropagatedContext.class
+ *     Additional class: io/opentelemetry/javaagent/instrumentation/internal/lambda/LambdaTransformer.class
+ *     Additional class: io/opentelemetry/javaagent/instrumentation/internal/lambda/Java9LambdaTransformer.class
+ *     Additional class: io/opentelemetry/javaagent/instrumentation/internal/reflection/ReflectionHelper.class
  */
 public class AdditionalClassesInjectorListener implements HelperInjectorListener {
 
